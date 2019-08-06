ccf0a75adbabd555e458d01a0a5351038a6b72d7
Alan <alanson76@gmail.com>
2019-08-06 22:16:56 +0900

    renewed html file

--- appserver/src/main/java/com/naver/eng/appserver/controller/MainController.java
+++ appserver/src/main/java/com/naver/eng/appserver/controller/MainController.java
@@ -3,6 +3,7 @@ package com.naver.eng.appserver.controller;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +11,7 @@ import com.naver.eng.appserver.model.Param;
 import com.naver.eng.appserver.service.MainService;
 
 
-@RestController("/")
+@Controller
 public class MainController 
 {
 	
@@ -20,6 +21,7 @@ public class MainController
 	@RequestMapping("/search")
     Object index(Map<String, Object> model) throws Exception {
 		Param param = new Param("aaa");
-		return mainService.getMessageList(param);
+		return "index";
+//		return mainService.getMessageList(param);
     }
 }
---
+++ appserver/src/main/resources/templates/index.ftl
@@ -0,0 +1,9 @@
+<html>
+<head>
+	<title>Team Hoban</title>
+</head>
+
+<body>
+	<h1>HELLO HOBAN</h1>
+</body>
+</html>
